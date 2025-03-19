71625f4de7a9e98a2b66ad478489221d0e359dcd
Elano <137415938+elano-vasconcelos-Vectorsolutions@users.noreply.github.com>
2025-03-19 10:54:13 +0000

    Initial project setup

---
+++ CoolReceipts/Features/List/ReceiptListView.swift
@@ -0,0 +1,18 @@
+//
+//  ReceiptListView.swift
+//  CoolReceipts
+//
+//  Created by Elano Vasconcelos on 19/03/25.
+//
+
+import SwiftUI
+
+struct ReceiptListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReceiptListView()
+}
--- CoolReceipts/SceneDelegate.swift
+++ CoolReceipts/SceneDelegate.swift
@@ -6,17 +6,31 @@
 //
 
 import UIKit
+import SwiftUI
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-
+//    var appCoordinator: AppCoordinator?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        let window = UIWindow(windowScene: windowScene)
+        let view = UIHostingController(rootView: ReceiptListView())
+        let navigationController = UINavigationController(rootViewController: view)
+        
+        
+        // Initialize the coordinator with the navigation controller and factory
+//        let viewFactory = ViewFactory(networkService: networkService)
+        
+//        appCoordinator = AppCoordinator(navigationController: navigationController, viewFactory: viewFactory)
+//        appCoordinator?.start()
+        
+        window.rootViewController = navigationController
+        window.makeKeyAndVisible()
+        
+        self.window = window
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
